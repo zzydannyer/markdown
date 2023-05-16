2f43abc8f7334ed5f11105010ac062b49e5b211e
张泽宇 <1404631613@qq.com>
2023-05-17 02:50:47 +0800

    乌鱼子

--- TestProGramme/my-vue-app/src/views/MousePosition.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>鼠标位置：X {{ x }} / Y {{ y }}</div>
-</template>
-
-<script lang="ts" setup>
-import useMousePosition from "../Hooks/mousePosition";
-
-const { x, y } = useMousePosition();
-</script>
